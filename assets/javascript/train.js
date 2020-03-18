688586cc48064d9a93a2a8c1f7e96342a28dad97
BrockH <brock.horton@gmail.com>
2020-03-18 15:06:05 -0400

    added base javascript for firbase link

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -5,3 +5,15 @@
 //calculate the times 
 
 //Firebase link
+var Config = {
+    apiKey: "AIzaSyD-yLzclO7OhzFvnjaqHFdjNUUWtMpIrug",
+    authDomain: "trainscheduler-b8efc.firebaseapp.com",
+    databaseURL: "https://trainscheduler-b8efc.firebaseio.com",
+    projectId: "trainscheduler-b8efc",
+    storageBucket: "trainscheduler-b8efc.appspot.com",
+};
+
+firebase.initializeApp(Config)
+
+// variable to reference the database
+var database = firebase.database();
